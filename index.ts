2f4454ab9ae54406b194220d26872e23b75860de
lorahmadt <ahmad.tahir12045@gmail.com>
2026-04-23 16:40:25 -0400

    first TypeScript file

---
+++ index.ts
@@ -0,0 +1,11 @@
+let age: number = 17
+let playerName: string = "Ahmad"
+let isLearning: boolean = true
+
+console.log(playerName, "is", age, "years old")
+
+function addKills(current: number, newKills: number): number {
+    return current + newKills
+}
+
+console.log(addKills(10, 5))
