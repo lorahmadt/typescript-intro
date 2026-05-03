aa2c5cdf0f2a70b2f662147509d5ff682dcce609
lorahmadt <ahmad.tahir12045@gmail.com>
2026-05-03 16:29:30 -0400

    Add CSS styling and formatted scores

--- real-madrid-tracker/app.js
+++ real-madrid-tracker/app.js
@@ -1,15 +1,20 @@
 async function getNextMatch() {
   const response = await fetch("http://localhost:3000/next-match");
   const match = await response.json();
-  const text = `${match.homeTeam.name} vs ${match.awayTeam.name} — ${match.utcDate}`;
+  const date = new Date(match.utcDate).toLocaleString('en-us', { dataStyle: 'long', timeStyle: 'short'});
+  const text = `${match.homeTeam.name} vs ${match.awayTeam.name} — ${date}`;
   document.getElementById("next-match").innerText = text;
 }
 
 async function getLastResult() {
   const response = await fetch("http://localhost:3000/last-result");
   const match = await response.json();
-  const text = `${match.homeTeam.name} ${match.score.fullTime.home} - ${match.score.fullTime.away} ${match.awayTeam.name}`;
-  document.getElementById("last-result").innerText = text;
+  const score = `${match.score.fullTime.home} - ${match.score.fullTime.away}`;
+  const teams = `${match.homeTeam.name} vs ${match.awayTeam.name}`;
+  document.getElementById("last-result").innerHTML = `
+    <p>${teams}</p>
+    <p style="font-size: 32px; color: #D4AF37; font-weight: bold; margin-top: 8px;">${score}</p>
+  `;
 }
 
 getNextMatch();
